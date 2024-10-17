ceb752bba5b11e2685702bd2c9b6a46bf6956e2e
T4ras123 <vovatara123@gmail.com>
2024-10-17 14:44:44 +0400

    feat: add SmallGPT model implementation and GPT4Tokenizer for text processing

--- SmallGPT/run.py
+++ SmallGPT/run.py
@@ -3,7 +3,7 @@ from gpt import SmallGPT, decode # Import the model from model.py
 
 m = SmallGPT()
 
-model_path = "model.pth"
+model_path = "../model.pth"
 
 # Load the model state dictionary
 m.load_state_dict(torch.load(model_path))
